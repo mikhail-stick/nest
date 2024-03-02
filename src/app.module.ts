cb351ceffa7cd6b12a28dad945af377b44ffc86d
misha-rab-ymniy <mihaildubinka@gmail.com>
2024-03-02 20:42:03 +0300

    add typing for configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,21 @@ import { UserModule } from './user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+interface DatabaseConfig {
+  POSTGRES_HOST: string;
+  POSTGRES_PORT: number;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: string;
+}
+
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env.development' }),
+    ConfigModule.forRoot({ envFilePath: '.env.development', isGlobal: true }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService<DatabaseConfig>) => ({
         type: 'postgres',
         host: configService.getOrThrow('POSTGRES_HOST'),
-        port: Number(configService.getOrThrow('POSTGRES_PORT')),
+        port: configService.getOrThrow('POSTGRES_PORT', { infer: true }),
         username: configService.getOrThrow('POSTGRES_USER'),
         password: configService.getOrThrow('POSTGRES_PASSWORD'),
         // entities: [User],
